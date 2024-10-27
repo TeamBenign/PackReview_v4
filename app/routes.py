275bd410e9d963322088bb4e40767b7f59cfc6e8
anish-dhage <anish.dhage@gmail.com>
2024-10-26 22:19:12 -0400

    refactoring routes file

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from app import app, db
 from app.models import Reviews
 from utils import get_DB
 
-jobsDB  = None
+jobsDB = None
 usersDb = None
 db = None
 
@@ -16,6 +16,7 @@ def intializeDB():
     usersDb = db.Users
     jobsDB = db.Jobs
 
+
 def process_jobs(job_list):
     processed = list()
     for job in job_list:
@@ -23,10 +24,10 @@ def process_jobs(job_list):
         processed.append(job)
     return processed
 
+
 def get_all_jobs():
     all_jobs = list(jobsDB.find())
     return process_jobs(all_jobs)
-
 
 
 def get_my_jobs(username):
@@ -38,7 +39,9 @@ def get_my_jobs(username):
     reviews = user['reviews']
     return process_jobs(jobsDB.find({"_id": {'$in': reviews}}))
 
-#create job review
+# create job review
+
+
 @app.route('/review')
 def review():
     """
@@ -51,7 +54,7 @@ def review():
     return render_template('review-page.html', entries=entries)
 
 
-#view all
+# view all
 @app.route('/pageContent')
 def page_content():
     """An API for the user to view all the reviews entered"""
@@ -61,54 +64,56 @@ def page_content():
     location_filter_entries = jobsDB.distinct("locations")
     title_filter_entries = jobsDB.distinct("title")
     company_filter_entries = jobsDB.distinct("company")
-    
-        #pagination
-        
-    #print(entries)
-    page, per_page, offset = get_page_args(page_parameter="page", per_page_parameter="per_page")
+
+    # pagination
+
+    # print(entries)
+    page, per_page, offset = get_page_args(
+        page_parameter="page", per_page_parameter="per_page")
     total = len(entries)
-    
+
     if not page or not per_page:
         offset = 0
         per_page = 10
         pagination_entries = entries[offset: offset+per_page]
     else:
         pagination_entries = entries[offset: offset+per_page]
-        #print("ELSE!!!")
+        # print("ELSE!!!")
 
     pagination = Pagination(page=page, per_page=per_page,
-                            total=total, css_framework = 'bootstrap4')
-    
+                            total=total, css_framework='bootstrap4')
+
     #
     # print("HERE!!!", pagination._get_single_page_link())
-    #return render_template('page_content.html', entries=pagination_entries, page=page, per_page=per_page, pagination=pagination)
-    return render_template('page_content.html', entries=pagination_entries, page=page, per_page=per_page, pagination=pagination,dept_filter_entries=dept_filter_entries,location_filter_entries=location_filter_entries,company_filter_entries=company_filter_entries)
+    # return render_template('page_content.html', entries=pagination_entries, page=page, per_page=per_page, pagination=pagination)
+    return render_template('page_content.html', entries=pagination_entries, page=page, per_page=per_page, pagination=pagination, dept_filter_entries=dept_filter_entries, location_filter_entries=location_filter_entries, company_filter_entries=company_filter_entries)
 
 
-#view all
+# view all
 @app.route('/myjobs')
 def myjobs():
     """An API for the user to view all the reviews created by them"""
     intializeDB()
     entries = get_my_jobs(session['username'])
-    page, per_page, offset = get_page_args(page_parameter="page", per_page_parameter="per_page")
+    page, per_page, offset = get_page_args(
+        page_parameter="page", per_page_parameter="per_page")
     total = len(entries)
-    
+
     if not page or not per_page:
         offset = 0
         per_page = 10
         pagination_entries = entries[offset: offset+per_page]
     else:
         pagination_entries = entries[offset: offset+per_page]
-        #print("ELSE!!!")
+        # print("ELSE!!!")
 
     pagination = Pagination(page=page, per_page=per_page,
-                            total=total, css_framework = 'bootstrap4')
-    
+                            total=total, css_framework='bootstrap4')
+
     return render_template('myjobs.html', entries=pagination_entries, page=page, per_page=per_page, pagination=pagination)
 
 
-#search
+# search
 @app.route('/pageContentPost', methods=['POST'])
 def page_content_post():
     """An API for the user to view specific reviews depending on the job title"""
@@ -116,13 +121,14 @@ def page_content_post():
     if request.method == 'POST':
         form = request.form
         search_title = form.get('search')
-        print("search is",search_title)
-        filter_entries= get_all_jobs()
+        print("search is", search_title)
+        filter_entries = get_all_jobs()
         if search_title.strip() == '':
             entries = get_all_jobs()
         else:
             print("s entered")
-            entries = process_jobs(jobsDB.find({"title": "/"+search_title+"/"}))
+            entries = process_jobs(jobsDB.find(
+                {"title": "/"+search_title+"/"}))
         dept_filter_entries = jobsDB.distinct("department")
         location_filter_entries = jobsDB.distinct("locations")
         title_filter_entries = jobsDB.distinct("title")
@@ -151,35 +157,39 @@ def page_content_post():
 
         if company_filter_title and dept_filter_title:
             print("dept filter is", dept_filter_title)
-            entries = process_jobs(jobsDB.find({"company":  { "$in": company_filter_title},"department":  { "$in": dept_filter_title} }))
+            entries = process_jobs(jobsDB.find({"company":  {
+                                   "$in": company_filter_title}, "department":  {"$in": dept_filter_title}}))
         elif dept_filter_title and not company_filter_title:
             print("location filter is", dept_filter_title)
-            entries = process_jobs(jobsDB.find({"department":  { "$in": dept_filter_title} }))
+            entries = process_jobs(jobsDB.find(
+                {"department":  {"$in": dept_filter_title}}))
         elif company_filter_title and not dept_filter_title:
             print("company filter is", company_filter_title)
-            entries = process_jobs(jobsDB.find({"company":  { "$in": company_filter_title} }))
-        page, per_page, offset = get_page_args(page_parameter="page", per_page_parameter="per_page")
+            entries = process_jobs(jobsDB.find(
+                {"company":  {"$in": company_filter_title}}))
+        page, per_page, offset = get_page_args(
+            page_parameter="page", per_page_parameter="per_page")
         total = len(entries)
-    
+
         if not page or not per_page:
             offset = 0
             per_page = 10
             pagination_entries = entries[offset: offset+per_page]
         else:
             pagination_entries = entries[offset: offset+per_page]
-            #print("ELSE!!!")
+            # print("ELSE!!!")
 
         pagination = Pagination(page=page, per_page=per_page,
-                            total=total, css_framework = 'bootstrap4')
+                                total=total, css_framework='bootstrap4')
 
         return render_template('page_content.html', entries=pagination_entries,
                                dept_filter_entries=dept_filter_entries,
-                               location_filter_entries=location_filter_entries, 
+                               location_filter_entries=location_filter_entries,
                                company_filter_entries=company_filter_entries, page=page, per_page=per_page, pagination=pagination)
+
 
 @app.route('/')
 @app.route('/home')
-
 def home():
     """An API for the user to be able to access the homepage through the navbar"""
     intializeDB()
@@ -201,32 +211,35 @@ def add():
 
         job = {
             "_id": form.get('job_title') + "_" + form.get('company') + "_" + form.get('locations'),
-                "title": form.get('job_title'),
-                "company": form.get('company'),
-                "description": form.get('job_description'),
-                "locations": form.get('locations'),
-                "department": form.get('department'),
-                "hourly_pay":   form.get('hourly_pay'),
-                "benefits": form.get('benefits'),
-                "review": form.get('review'),
-                "rating": form.get('rating'),
-                "recommendation": form.get('recommendation'),
-                "author": session['username'],
-                "upvote": 0
-            }
+            "title": form.get('job_title'),
+            "company": form.get('company'),
+            "description": form.get('job_description'),
+            "locations": form.get('locations'),
+            "department": form.get('department'),
+            "hourly_pay":   form.get('hourly_pay'),
+            "benefits": form.get('benefits'),
+            "review": form.get('review'),
+            "rating": form.get('rating'),
+            "recommendation": form.get('recommendation'),
+            "author": session['username'],
+            "upvote": 0
+        }
 
-        if jobsDB.find_one({'_id': job['_id']})== None:
+        if jobsDB.find_one({'_id': job['_id']}) == None:
             jobsDB.insert_one(job)
             reviews.append(job['_id'])
-            usersDb.update_one({"username": session['username']}, {"$set": {"reviews": reviews}})
+            usersDb.update_one({"username": session['username']}, {
+                               "$set": {"reviews": reviews}})
 
     return redirect('/')
 
+
 @app.route('/logout')
 def logout():
-   """An API to help users logout"""
-   session.pop('username', None)
-   return redirect('/')
+    """An API to help users logout"""
+    session.pop('username', None)
+    return redirect('/')
+
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
@@ -238,7 +251,7 @@ def login():
     if request.method == "POST":
         username = request.form.get("username")
         user = usersDb.find_one({"username": username})
-        passw =  request.form.get("password")
+        passw = request.form.get("password")
 
         if user and user["password"] == passw:
             session["username"] = username
@@ -248,20 +261,21 @@ def login():
 
     return render_template("login.html")
 
+
 @app.route("/signup", methods=["POST", "GET"])
 def signup():
     """An API to help users signup"""
     intializeDB()
     if 'username' in session.keys() and session['username']:
-        print("User ", session['username']," already logged in")
+        print("User ", session['username'], " already logged in")
         return redirect("/")
-        
+
     if request.method == "POST":
         username = request.form.get("username")
         user = usersDb.find_one({"username": username})
-        passw =  request.form.get("password")
+        passw = request.form.get("password")
 
-        #new user
+        # new user
         if not user:
             user = {
                 "username": username,
@@ -269,11 +283,12 @@ def signup():
                 "reviews": list()
             }
             usersDb.insert_one(user)
-            
+
         session["username"] = username
         return redirect("/")
 
     return render_template("signup.html")
+
 
 @app.route('/view/<id>')
 def view(id):
@@ -283,15 +298,17 @@ def view(id):
     jobReview['id'] = jobReview.pop('_id')
     return render_template("view.html", entry=jobReview)
 
+
 @app.route('/upvote/<id>')
 def upvote(id):
     """An API to update upvote information"""
     intializeDB()
     jobReview = jobsDB.find_one({"_id": id})
     upvote = jobReview['upvote']
-    upvote+=1
+    upvote += 1
     jobsDB.update_one({"_id": id}, {"$set": {"upvote": upvote}})
     return redirect("/view/"+id)
+
 
 @app.route('/downvote/<id>')
 def downvote(id):
@@ -299,10 +316,9 @@ def downvote(id):
     intializeDB()
     jobReview = jobsDB.find_one({"_id": id})
     upvote = jobReview['upvote']
-    upvote-=1
+    upvote -= 1
     jobsDB.update_one({"_id": id}, {"$set": {"upvote": upvote}})
     return redirect("/view/"+id)
-
 
 
 @app.route('/delete/<id>')
@@ -315,7 +331,8 @@ def delete(id):
 
     reviews = user['reviews']
     reviews.remove(id)
-    usersDb.update_one({"username": session['username']}, {"$set": {"reviews": reviews}})
+    usersDb.update_one({"username": session['username']}, {
+                       "$set": {"reviews": reviews}})
 
-    jobsDB.delete_one( {"_id":id })
+    jobsDB.delete_one({"_id": id})
     return redirect("/myjobs")
--- utils.py
+++ utils.py
@@ -2,10 +2,11 @@ import os
 import sys
 from pymongo import MongoClient
 
+
 def get_DB():
     with open(os.path.join(sys.path[0], "config.ini"), "r") as f:
-        content=f.readlines()
-    client = MongoClient("mongodb+srv://ncsuse123:"+content[0]+"@cluster0.vceghwv.mongodb.net/?retryWrites=true&w=majority")
+        content = f.readlines()
+    client = MongoClient("mongodb+srv://anishd1910:" +
+                         content[0]+"@cluster0.oagwk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-    
     return client.SEProj2
