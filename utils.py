36dff2b5298f474d1d8a4d05f051491f51a6d4ec
anish-dhage <anish.dhage@gmail.com>
2024-10-30 23:52:49 -0400

    TestWorkflow

--- utils.py
+++ utils.py
@@ -1,15 +1,13 @@
 import os
 import sys
 from pymongo import MongoClient
-import ssl
+
 
 def get_DB(isTest=False):
     with open(os.path.join(sys.path[0], "config.ini"), "r") as f:
         content = f.readlines()
     client = MongoClient("mongodb+srv://anishd1910:" +
-                         content[0]+"@cluster0.oagwk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-                                 tls=True,  # Enable TLS (SSL)
-                                     tlsAllowInvalidCertificates=True)
+                         content[0]+"@cluster0.oagwk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
     if (isTest):
         return client.SETestProj
