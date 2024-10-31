1792429a0e09d2e601c3c6654fd33ce3402d26e4
anish-dhage <anish.dhage@gmail.com>
2024-10-30 23:27:22 -0400

    Utils url update

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ def get_DB(isTest=False):
     with open(os.path.join(sys.path[0], "config.ini"), "r") as f:
         content = f.readlines()
     client = MongoClient("mongodb+srv://anishd1910:" +
-                         content[0]+"@cluster0.oagwk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+                         content[0]+"@cluster0.oagwk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&ssl=true")
 
     if (isTest):
         return client.SETestProj
